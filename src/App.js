bc188101c764355bca0c23fdaf1591c166b5cdee
yksvSG <sg.yksv77@gmail.com>
2022-03-13 00:12:29 +0900

    컴포넌트 재사용-상태 끌어올리기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
+import Board from "./Board";
 import { useState } from "react";
 import Button from "./Button";
-import Dice from "./Dice";
 
 function random(n) {
   return Math.ceil(Math.random() * n);
@@ -10,21 +10,28 @@ function App() {
   const [num, setNum] = useState(1);
   const [sum, setSum] = useState(0);
   const [gameHistory, setGameHistory] = useState([]);
+  const [otherNum, setOtherNum] = useState(1);
+  const [otherSum, setOtherSum] = useState(0);
+  const [otherGameHistory, setOtherGameHistory] = useState([]);
 
   const handleRollClick = () => {
     const nextNum = random(6);
+    const nextOtherNum = random(6);
     setNum(nextNum);
     setSum(sum + nextNum);
-    // 잘못된 코드 - 참조형 State 잘못된 예시
-    // gameHistory.push(nextNum);
-    // setGameHistory(gameHistory);
     setGameHistory([...gameHistory, nextNum]);
+    setNum(nextOtherNum);
+    setSum(otherSum + nextOtherNum);
+    setGameHistory([...otherGameHistory, nextOtherNum]);
   };
 
   const handleClearClick = () => {
     setNum(1);
     setSum(0);
     setGameHistory([]);
+    setOtherNum(1);
+    setOtherSum(0);
+    setOtherGameHistory([]);
   };
 
   return (
@@ -34,12 +41,20 @@ function App() {
         <Button onClick={handleClearClick}>처음부터</Button>
       </div>
       <div>
-        <h2>나</h2>
-        <Dice color="red" num={num} />
-        <h2>총점</h2>
-        <p>{sum}</p>
-        <h2>기록</h2>
-        <p>{gameHistory.join(", ")}</p>
+        <Board
+          name="나"
+          color="blue"
+          num={num}
+          sum={sum}
+          gameHistory={gameHistory}
+        />
+        <Board
+          name="상대"
+          color="red"
+          num={otherNum}
+          sum={otherSum}
+          gameHistory={otherGameHistory}
+        />
       </div>
     </div>
   );
---
+++ src/Board.js
@@ -0,0 +1,18 @@
+import Dice from "./Dice";
+
+function Board({ name, color, num, sum, gameHistory }) {
+  return (
+    <div>
+      <div>
+        <h2>{name}</h2>
+        <Dice color={color} num={num} />
+        <h2>총점</h2>
+        <p>{sum}</p>
+        <h2>기록</h2>
+        <p>{gameHistory.join(", ")}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Board;
