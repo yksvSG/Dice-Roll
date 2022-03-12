5e5fc3ce00228c4342f6b8708706458bfd3115ba
yksvSG <sg.yksv77@gmail.com>
2022-03-12 20:35:39 +0900

    State

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { useState } from "react";
+import Button from "./Button";
 import Dice from "./Dice";
 
+function random(n) {
+  return Math.ceil(Math.random() * n);
+}
+
 function App() {
+  const [num, setNum] = useState(1);
+
+  const handleRollClick = () => {
+    const nextNum = random(6);
+    setNum(nextNum);
+  };
+
+  const handleClearClick = () => {
+    setNum(1);
+  };
+
   return (
     <div>
-      <Dice color="red" num={2} />
+      <div>
+        <Button onClick={handleRollClick}>던지기</Button>
+        <Button onClick={handleClearClick}>처음부터</Button>
+      </div>
+      <Dice color="red" num={num} />
     </div>
   );
 }
